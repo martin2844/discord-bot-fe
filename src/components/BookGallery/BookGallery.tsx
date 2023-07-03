622d4cfa02b85eeece12affc354586c69b9bc0e0
Martin Chammah <martin2844@gmail.com>
2023-07-03 15:45:17 -0300

    Add Key

--- src/components/BookGallery/BookGallery.tsx
+++ src/components/BookGallery/BookGallery.tsx
@@ -6,8 +6,8 @@ const BookGallery = async () => {
    return (
       <div className="flex flex-wrap justify-center lg:w-full xl:w-3/4 p-12">
          {books.map((b) => (
-            <div className="w-full lg:w-1/2 p-12">
-               <Card key={b.id} book={b} />
+            <div key={b.book_id} className="w-full lg:w-1/2 p-12">
+               <Card key={b.book_id} book={b} />
             </div>
          ))}
       </div>
